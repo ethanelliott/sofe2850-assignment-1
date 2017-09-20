8400dd4b15d0b4a29252d2826299e76f3b6182b2
Ethan Elliott <ethan.elliott@hotmail.ca>
2017-09-20 16:59:05 -0400

    Formatting

--- main.h
+++ main.h
@@ -1,25 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#ifndef MAINCLASS_MACRO
-#define MAINCLASS_MACRO
-
 #define START_MAIN_CLASS(_ClassName_) \
 	_ClassName_ * _ClassName_::instance = NULL; \
-	int main() \
-	{ \
+	int main() { \
 		_ClassName_ *base =  _ClassName_::GetInstance(); \
-		try  \
-		{ \
+		try { \
 			throw(base->Main()); \
-		} \
-		catch(int e){ \
+		} catch(int e) { \
 			cout << "\nProgram returned " << e << "\n"; \
 			return e; \
 		} \
 		return 0; \
 	}
-#endif
 
 #include <iostream>
 #include "assignment.h"
